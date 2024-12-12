93b20124b49df78cb72ed70251799a00d89395c8
EfratGar <efratgar@gmail.com>
2024-12-12 19:42:41 +0200

    More animations

---
+++ Assets/Scripts/BK Animation/AzzaAnimation.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class SeaWeedAnimation : MonoBehaviour
+{
+    [Header("Breathing Animation Settings")]
+    [SerializeField] private float scaleAmplitude = 0.05f; 
+    [SerializeField] private float scaleSpeed = 2.0f;
+
+    [Header("Sway Animation Settings")]
+    [SerializeField] private float swayAmplitude = 0.005f;
+    [SerializeField] private float swaySpeed = 1f; 
+
+    private Vector3 startScale;
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startScale = transform.localScale; 
+        startPosition = transform.position; 
+    }
+
+    void Update()
+    {
+        float scaleOffset = Mathf.Sin(Time.time * scaleSpeed) * scaleAmplitude;
+        transform.localScale = new Vector3(startScale.x, startScale.y + scaleOffset, startScale.z);
+
+        transform.position = new Vector3(startPosition.x, startPosition.y, startPosition.z);
+
+    }
+}
