3e20884f82d8ec75e90a2222c14b90cb65850f44
Esty212 <esty.dziubov@gmail.com>
2024-12-22 22:49:13 +0200

    Implemented Coin Animation and DOTween

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class Coin : MonoBehaviour
@@ -6,6 +7,7 @@ public class Coin : MonoBehaviour
     private bool hasTriggered;
 
     private CoinManager coinManager;
+    private Tweener _coinFallTween;
 
     Rigidbody2D rb;
 
@@ -13,6 +15,8 @@ public class Coin : MonoBehaviour
     {
         coinManager = CoinManager.instance;
         rb = GetComponent<Rigidbody2D>();
+        CoinFall();
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -25,9 +29,25 @@ public class Coin : MonoBehaviour
         }
         else if (collision.CompareTag("Ground"))
         {
+            _coinFallTween.Kill();
+            transform.rotation = Quaternion.identity;
+            transform.DORotate(new Vector3(0f, 360f, 0f), 2f, RotateMode.FastBeyond360)
+                .SetLoops(-1, LoopType.Restart)
+                .SetRelative()
+                .SetEase(Ease.Linear);
+
             rb.velocity = Vector2.zero;
             rb.bodyType = RigidbodyType2D.Static;
         }
+    }
+
+    private void CoinFall()
+    {
+        _coinFallTween = transform.DORotate(new Vector3(360f, 0f, 0f), 2f, RotateMode.FastBeyond360)
+        .SetLoops(-1, LoopType.Restart)
+        .SetRelative()
+        .SetEase(Ease.Linear);
+
     }
 
 }
